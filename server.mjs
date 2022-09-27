dc2c6407bb57e8e7dc7f6b94ff3833d6bde861e3
Noman Waseem <nomanwaseem111@gmail.com>
2022-09-28 03:10:50 +0500

    final changes

--- server.mjs
+++ server.mjs
@@ -30,51 +30,54 @@ app.post('/user', (req, res) => {
      }
 
      
-     userModel.findOne({password:body.password} , (err,data) => {
-
-      if(!err){
-  
-         console.log("data :", data);
-
-         if(data){
-            console.log(" User is Already Exist :", data);
-            res.status(401).send({message : "User is Already Exist"})
-            return;
-        }else{
+     userModel.findOne({email:body.email} , (error,data) => {
 
 
-          stringToHash(body.password).then(hashString => {
+      if(!error){
 
+          console.log("data :", data);
+
+          if(data){
               
-              let newUser = new userModel({
+             console.log("User is Already Exist :", data);
+             res.status(400).send({message : "User is Already Exist"})
+             return;
 
-                email : body.email.toLowerCase(),
-                password : hashString
+          }else{
+
+              stringToHash(body.password).then(hashString => {
+             
+                 let newUser = new userModel ({
+
+                    email : body.email.toLowerCase(),
+                    password : hashString
+
+                 })
+                 newUser.save((err,result) => {
+                    
+                    if(!err){
+                      console.log("Data Saved :", result);
+                      res.status(201).send({message : "User is Signup"})
+
+                    }else{
+                      console.log("db error ", err);
+                      res.status(500).send({message : "Internal Server Error"})
+                    }
+
+                 })
+
 
               })
-              newUser.save((err,result) => {
-                       
-                if(!err){
-                  console.log("Data Saved in db", result);
-                  res.status(201).send({message : "User is Signup"});
-                }else{
-                  console.log("db error :" , err);
-                  res.status(500).send({message : "Internal Sever Error"})
-                }
+          }
 
-              })
-
-          })
-          
-         }
-         }else{
-        console.log("db error in query", err);
-        res.status(500).send({message : "Internal db error"})
-
+      }else{
+        console.log("db error :", error);
+        res.status(500).send({message : "db error in saved data"})
       }
 
+
+
      })
-     
 
 
   })
